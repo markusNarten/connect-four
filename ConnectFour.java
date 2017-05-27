ffad12f589199478c73a7ad745e5221ccadff5f1
18hellinga <18hellinga@students.da.org>
2017-05-27 14:55:50 -0400

    first bit of work

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,1 +1,14 @@
 
+
+public class ConnectFour implements BoardGame{
+	private int[][] board;
+	private int currentPlayer;
+	private Position[] winningPositions;
+	
+	
+	
+	
+	public static void main(String args[]){
+		
+	}
+}
