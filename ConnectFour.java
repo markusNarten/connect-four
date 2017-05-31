f343d8d97b4c2e832f9c91800d7b42dd83da22f6
markusNarten <markus.narten@gmail.com>
2017-05-30 22:27:52 -0400

    final changes

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,16 +1,30 @@
-
+/**
+ * a connect four game
+ * @author 18helling/18narten
+ *@version 5/30/2017
+ */
 public class ConnectFour implements BoardGame {
 	private int[][] board;
 	private int currentPlayer;
 	private Position[] winningPositions;
 	private int winner;
 
+	/**
+	 * default constructor for a connect four game
+	 */
 	public ConnectFour() {
 		board = new int[6][7];
 		winningPositions = new Position[4];
+		winningPositions[0] = new Position();
+		winningPositions[1] = new Position();
+		winningPositions[2] = new Position();
+		winningPositions[3] = new Position();
 		currentPlayer = 1;
 	}
 
+	/**
+	 * generates a new game of connect four
+	 */
 	public void newGame() {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
@@ -19,10 +33,18 @@ public class ConnectFour implements BoardGame {
 		}
 		winner = 0;
 		winningPositions = new Position[4];
+		winningPositions[0] = new Position();
+		winningPositions[1] = new Position();
+		winningPositions[2] = new Position();
+		winningPositions[3] = new Position();
 		currentPlayer = 1;
 
 	}
 
+	/**
+	 * determines if the game has ended
+	 * @return if the game is over
+	 */
 	public boolean gameOver() {
 		if (getWinner() != 0) {
 			winner = getWinner();
@@ -34,62 +56,77 @@ public class ConnectFour implements BoardGame {
 						return false;
 				}
 			}
+			return true;
 		}
-		return true;
+		
 	}
 
+	/**
+	 * determines if there is a winner for the game
+	 * @return the winner, 0 if no winner
+	 */
 	public int getWinner() {
-		for (int r = 0; r < board.length; r++) {
-			for (int c = 0; c < board[r].length; c++) {
-				if (board[r][c] == currentPlayer) {
-					for (int vert = -1; vert < 2; vert++) {
-						for (int horiz = -1; horiz < 2; horiz++) {
-							if ((r + (3 * vert) > 0) && (c + (3 * horiz) > 0) && (r + (3 * vert) < 6)
-									&& (c + (3 * horiz) < 7) && ((vert != 0 || horiz != 0))
-									&& (board[r + vert][c + horiz] == currentPlayer)
-									&& (board[r + vert * 2][c + horiz * 2] == currentPlayer)
-									&& (board[r + vert * 3][c + horiz * 3] == currentPlayer)) {
-								winningPositions[0] = new Position(r, c);
-								winningPositions[1] = new Position(r + vert, c + horiz);
-								winningPositions[2] = new Position(r + (2 * vert), c + (2 * horiz));
-								winningPositions[3] = new Position(r + (3 * vert), c + (3 * horiz));
-								return currentPlayer;
+		for(int p = 1; p< 3;p++)
+			for (int r = 0; r < board.length; r++) {
+				for (int c = 0; c < board[r].length; c++) {
+					if (board[r][c] == p) {
+						for (int vert = -1; vert < 2; vert++) {
+							for (int horiz = -1; horiz < 2; horiz++) {
+								if ((r + (3 * vert) > 0) && (c + (3 * horiz) > 0) && (r + (3 * vert) < 6)
+										&& (c + (3 * horiz) < 7) && ((vert != 0 || horiz != 0))
+										&& (board[r + vert][c + horiz] == p)
+										&& (board[r + vert * 2][c + horiz * 2] == p)
+										&& (board[r + vert * 3][c + horiz * 3] == p)) {
+									winningPositions[0] = new Position(r, c);
+									winningPositions[1] = new Position(r + vert, c + horiz);
+									winningPositions[2] = new Position(r + (2 * vert), c + (2 * horiz));
+									winningPositions[3] = new Position(r + (3 * vert), c + (3 * horiz));
+									return p;
+								}
+	
 							}
-
 						}
 					}
 				}
+	
 			}
-
-		}
 		return 0;
 	}
 
+	/**
+	 * returns the winning positions
+	 * @return the positions
+	 */
 	public Position[] getWinningPositions() {
 		return winningPositions;
 	}
 
 	public boolean columnFull(int column) {
-		if (board[5][column] == 0)
-			return false;
-		return true;
+		return (board[0][column] != 0);
 	}
-
-	// Plays the game and checks if someone's won
+ /**
+  * Plays the game and checks if someone's won
+  */
 	public void play(int column) {
 		if (!columnFull(column)) {
-			for (int i = 0; i < 6; i++)
+			for (int i = board.length-1; i >= 0; i--){
 				if (board[i][column] == 0) {
 					if (currentPlayer == 1) {
 						board[i][column] = 1;
 					}
-				} else {
+				 else {
 					board[i][column] = 2;
+					break;
 				}
+			}
+			currentPlayer = currentPlayer % 2 + 1;
 		}
-		currentPlayer = currentPlayer % 2 + 1;
 	}
 
+	/**
+	 * returns the boardstate
+	 * @return the board
+	 */
 	public int[][] getBoard() {
 		return board;
 	}
