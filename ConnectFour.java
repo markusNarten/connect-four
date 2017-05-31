77b2106b1e4e6bd8d8230e0381a18506d3faa72c
18hellinga <18hellinga@students.da.org>
2017-05-30 21:48:08 -0400

    Final(?)
    Well it’s fucked but it’s what i have

--- ConnectFour.java
+++ ConnectFour.java
@@ -18,53 +18,51 @@ public class ConnectFour implements BoardGame {
 			}
 		}
 		winner = 0;
+		winningPositions = new Position[4];
+		currentPlayer = 1;
 
 	}
 
 	public boolean gameOver() {
-		if(getWinner() != 0){
+		if (getWinner() != 0) {
 			winner = getWinner();
 			return true;
-		}
-		else if{
-			for(int row = 0; row < 6; row ++){
-				for(int col = 0; col < 7; col ++){
+		} else {
+			for (int row = 0; row < 6; row++) {
+				for (int col = 0; col < 7; col++) {
 					if (board[row][col] == 0)
-						return false
+						return false;
 				}
 			}
 		}
-		else{
-			return true;
-		}
+		return true;
 	}
 
-	public boolean winningPlay(){
-		for(int r = 0; r < board.length; r++){
-			for(int c = 0; c < board[r].length; c++){
-				if(board[r][c] == currentPlayer){
-					for(int vert = -1; vert < 2; vert++){
-						for(int horiz = -1; horiz < 2; horiz++){
-							if(r+(3*vert) > 0 && c+(3*horiz) > 0 && r+(3*vert) < 6 && c+(3*horiz) < 7 &&
-									(vert != 0 || horiz != 0) && board[r+vert][c+horiz] == currentPlayer &&
-									board[r+vert*2][c+horiz*2] == currentPlayer && board[r+vert*3][c+horiz*3 == currentPlayer){
+	public int getWinner() {
+		for (int r = 0; r < board.length; r++) {
+			for (int c = 0; c < board[r].length; c++) {
+				if (board[r][c] == currentPlayer) {
+					for (int vert = -1; vert < 2; vert++) {
+						for (int horiz = -1; horiz < 2; horiz++) {
+							if ((r + (3 * vert) > 0) && (c + (3 * horiz) > 0) && (r + (3 * vert) < 6)
+									&& (c + (3 * horiz) < 7) && ((vert != 0 || horiz != 0))
+									&& (board[r + vert][c + horiz] == currentPlayer)
+									&& (board[r + vert * 2][c + horiz * 2] == currentPlayer)
+									&& (board[r + vert * 3][c + horiz * 3] == currentPlayer)) {
 								winningPositions[0] = new Position(r, c);
-								winningPositions[1] = new Position(r+vert, c+horiz);
-								winningPositions[2] = new Position(r+(2*vert), c+(2*horiz));
-								winningPositions[3] = new Position(r+(3*vert), j+(3*horiz));
+								winningPositions[1] = new Position(r + vert, c + horiz);
+								winningPositions[2] = new Position(r + (2 * vert), c + (2 * horiz));
+								winningPositions[3] = new Position(r + (3 * vert), c + (3 * horiz));
+								return currentPlayer;
 							}
+
 						}
 					}
 				}
 			}
-			
-		}
-		return false;
-	}
 
-	public int getWinner() {
-		if winningPlay
-		return currentPlayer;
+		}
+		return 0;
 	}
 
 	public Position[] getWinningPositions() {
@@ -84,27 +82,15 @@ public class ConnectFour implements BoardGame {
 				if (board[i][column] == 0) {
 					if (currentPlayer == 1) {
 						board[i][column] = 1;
-						if (winningPlay(i, column)) {
-							getWinningPositions();
-							getWinner();
-						}
-					} else {
-						board[i][column] = 2;
-						if (winningPlay(i, column)) {
-							getWinningPositions();
-							getWinner();
-						}
 					}
-					currentPlayer = (currentPlayer + 1) % 2;
+				} else {
+					board[i][column] = 2;
 				}
 		}
+		currentPlayer = currentPlayer % 2 + 1;
 	}
 
 	public int[][] getBoard() {
-		int[][] temp = new int[6][7];
-		for (int a = 0; a < board.length; a++)
-			for (int b = 0; b < board[a].length; b++)
-				temp[a][b] = board[a][b];
-		return temp;
+		return board;
 	}
 }
